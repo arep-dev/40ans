6797807980a01f3f66aa2b0f38158f56fe8d6c80
arep-dev <g.azizi@arep.co.com>
2016-05-31 12:45:08 +0200

    Suppression de l'instance PDO

--- classes/Bdd.class.php
+++ classes/Bdd.class.php
@@ -4,7 +4,4 @@
 
 		public function __construc() {}
 	}
-
-	Bdd:$bdd = new PDO('mysql:host=142.4.214.101;dbname=40ans', 'arep_CATB', 'arep-sa');
-	MyModel::$bdd->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION );
 ?>
