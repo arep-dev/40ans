9072e11b1f1fb4be3b5c7889c9aa6d095cb9682e
arep-dev <g.azizi@arep.co.com>
2016-05-31 12:45:35 +0200

    Ajout de l'instance PDO

--- index.php
+++ index.php
@@ -5,10 +5,13 @@
     $loader = new Twig_Loader_Filesystem('views'); // Dossier contenant les templates
     $twig = new Twig_Environment($loader, array(
       'cache' => false
-    ));
+    )); 
 
     require('classes/Bdd.class.php');
     require('classes/Profil.class.php');
+
+    Bdd::$bdd = new PDO('mysql:host=142.4.214.101;dbname=40ans', 'arep_CATB', 'arep-sa');
+	Bdd::$bdd->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION );
 
     include('views/header.tpl');
 
