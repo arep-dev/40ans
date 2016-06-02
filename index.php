3ac29fbac488906cca20dd5bdbacb5eff1170dee
arep-dev <g.azizi@arep.co.com>
2016-06-02 11:24:44 +0200

    Ajout du clique pour appeler la methode checkin

--- index.php
+++ index.php
@@ -1,28 +1,42 @@
 <?php
+    ini_set('display_errors','on');
+    error_reporting(E_ALL);
     include_once('twig/lib/Twig/Autoloader.php');
-    Twig_Autoloader::register();
-    
-    $loader = new Twig_Loader_Filesystem('views'); // Dossier contenant les templates
-    $twig = new Twig_Environment($loader, array(
-      'cache' => false
-    )); 
-
     require('classes/Bdd.class.php');
     require('classes/Profil.class.php');
 
     Bdd::$bdd = new PDO('mysql:host=142.4.214.101;dbname=40ans', 'arep_CATB', 'arep-sa');
-	Bdd::$bdd->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION );
-
-    include('views/header.tpl');
-
-    $instance = new Profil();
-    $profils = $instance->getAllProfil();
+    Bdd::$bdd->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION );
 
 
-    echo $twig->render('body.tpl', array(
-    	'profils' => $profils
-    ));
+    if($_SERVER['REQUEST_METHOD'] == 'POST') {
+
+        $profils = $_POST['profil'];
+        $instance = new Profil();
+        $instance->setCheckIn($profils);
+        echo json_encode($_POST['profil']);
+
+    }else{
+
+        Twig_Autoloader::register();
+        
+        $loader = new Twig_Loader_Filesystem('views'); // Dossier contenant les templates
+        $twig = new Twig_Environment($loader, array(
+          'cache' => false
+        )); 
+
+        include('views/header.tpl');
+
+        $instance = new Profil();
+        $profils = $instance->getAllProfil();
 
 
-    include('views/footer.tpl');
+        echo $twig->render('body.tpl', array(
+        	'profils' => $profils
+        ));
+
+
+        include('views/footer.tpl');
+
+    }
 ?> 
--- js/script.js
+++ js/script.js
@@ -1,3 +1,40 @@
 $(function(){
-	// Start script
+
+	$('.list-container-profil').click(function(){
+
+		 checkVisibility = $(this).find('.check').attr('class');
+		 test = checkVisibility.substr( checkVisibility.lastIndexOf(' ') + 1);
+	
+		 console.log(test);
+			if(test == 'actif') {
+				$(this).find('.check').removeClass('actif');
+				$(this).find('input').attr('value', 0);
+			}else{
+				$(this).find('.check').addClass('actif');
+				$(this).find('input').attr('value', 1);
+
+			}	
+
+
+			checkin();
+				
+	});
+
+	function checkin() {
+		$.ajax({
+			url: 'index.php',
+			method: 'POST',
+			data: $('form').serialize(),
+			dataType: 'json',
+
+			success: function(result) {
+				console.log(result);
+			},
+
+			error: function(a, b, c) {
+				console.log(a+ ' '+b+' '+c);
+			}
+		});
+	}
+
 });
