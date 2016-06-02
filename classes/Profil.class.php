0a8ac474922600a2bc1ea2076b59931e82cff846
arep-dev <g.azizi@arep.co.com>
2016-06-02 11:23:58 +0200

    Ajout de la methode checkin

--- classes/Profil.class.php
+++ classes/Profil.class.php
@@ -1,4 +1,5 @@
 <?php 
+
 	class Profil extends Bdd {
 
 		public function __construct() {
@@ -6,15 +7,25 @@
 		}
 
 		public function getAllProfil() {
-			$request = Bdd::$bdd->query('SELECT * FROM PROFIL');
+			$request = Bdd::$bdd->query('SELECT * FROM PROFIL ORDER BY PAX_NOM');
 			$profils = $request->fetchAll();
 			return $profils;
 		}
 
 		public function getProfilById($id) {
-			$request = Bdd::$bdd->query('SELECT * FROM PROFIL WHERE PAX_NUM_FICHE = "'.$id.'"');
+			$request = Bdd::$bdd->query('SELECT * FROM PROFIL WHERE PAX_NUM_FICHE = "'.$id.'" ORDER BY PAX_NOM');
 			$profils = $request->fetchAll();
 			return $profils;
 		}
+
+		public function setCheckIn($profils) {
+			foreach($profils as $id => $statut) {
+				$request = 'UPDATE PROFIL SET PAX_CHECKIN = ?, PAX_HEURE_ARRIVEE = DATE_ADD(NOW(), INTERVAL 5 MINUTE) WHERE PAX_NUM_FICHE = ?';
+				$prepare = Bdd::$bdd->prepare($request);
+				$prepare->execute(array($statut, $id));
+			}
+		}
 	}
+
+
 ?>
